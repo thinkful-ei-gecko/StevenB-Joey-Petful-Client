7685cb29ad8f6b278f2e533a38ed147ea76b7667
Steven Bull <stevenwbulljr@gmail.com>
2019-11-15 19:48:36 +0000

    add dummy data

---
+++ src/Utils/dummyQueue.js
@@ -0,0 +1,20 @@
+module.exports = {
+ 'people': [
+  {
+    position: 1,
+    name: 'Steven'
+  },
+  {
+    position: 2,
+    name: 'Joey'
+  },
+  {
+    position: 3,
+    name: 'Anna'
+  },
+  {
+    position: 4,
+    name: 'Dave'
+  }
+ ]
+}

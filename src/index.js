412bca61ebb62cb902c401675d619260b3624a18
Steven Bull <stevenwbulljr@gmail.com>
2019-11-15 17:32:29 +0000

    small syntax tweaks

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import {AnimalProvider} from './Context/context';
+import { AnimalProvider } from './Context/context';
 
 ReactDOM.render(
 <BrowserRouter>
-<AnimalProvider>
-    <App />
-</AnimalProvider>
+  <AnimalProvider>
+      <App />
+  </AnimalProvider>
 </BrowserRouter>,
      document.getElementById('root'));
 
